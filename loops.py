570b168b21d3a42410bd3e937e91272e92769120
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2020-11-14 20:54:39 -0500

    Added loops: for and while

---
+++ loops.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 
+
+"""
+
+for i in range(11,21):
+    print("Cube of number ", i, " is ", i**3)
+    
+for a in (10,20,21.23234,2341,13241342,1112,"Text"):
+    print(a)
+
+i = 0
+while i <= 10:
+    print(i)
+    i = i + 1
