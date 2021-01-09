979a9eee7fe952ec7161100843ca04ec86a087c8
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2021-01-09 11:48:40 -0500

    Taking into the accounts keys

--- pygame_tutorial.py
+++ pygame_tutorial.py
@@ -16,18 +16,24 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
 
 isRunning = True
+x = 300
 while isRunning:
+    SURFACE.fill(BLACK)
+    
     for event in pygame.event.get():
         if event.type == QUIT:
             isRunning = False
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                x = x - 25
             
-            
-    pygame.draw.rect(SURFACE, RED, pygame.Rect(100, 50, 20, 60))
-    pygame.draw.rect(SURFACE, GREEN, pygame.Rect(300, 200, 50, 50), 10)
-    pygame.draw.circle(SURFACE, BLUE, (500, 500), 30)
-    pygame.draw.line(SURFACE, WHITE, (100,100), (200,200), 5)
+    #pygame.draw.rect(SURFACE, RED, pygame.Rect(100, 50, 20, 60))
+    pygame.draw.rect(SURFACE, GREEN, pygame.Rect(x, 200, 50, 50), 10)
+    #pygame.draw.circle(SURFACE, BLUE, (500, 500), 30)
+    #pygame.draw.line(SURFACE, WHITE, (100,100), (200,200), 5)
          
     pygame.display.update()
     
