846f50ab0dd3bd4095332c8b910eeaac63f73987
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2021-02-06 21:13:06 -0500

    Changed and simplified program a bit.

--- pygame_rock_scissors_paper.py
+++ pygame_rock_scissors_paper.py
@@ -6,6 +6,7 @@ Created on
 
 import pygame
 from pygame.locals import *
+import random
 
 pygame.init()
 
@@ -20,14 +21,44 @@ BLACK = (0, 0, 0)
 
 myfont = pygame.font.SysFont("Comic Sans MS", 30)
 text = myfont.render("What is your choice?", False, BLUE)
+textRect = text.get_rect()
+textRect.left = 50
+textRect.top = 50
 
 mainImage = pygame.image.load("rock_scissors_paper.png")
 scissors = mainImage.subsurface((0, 0, 1060, 750))
 scissors = pygame.transform.scale(scissors, (300, 200))
+scissorsRect = scissors.get_rect()
+scissorsRect.left = 50
+scissorsRect.top = 100
+
 rock = mainImage.subsurface((1600, 300, 2400-1600, 1000-300))
 rock = pygame.transform.scale(rock, (300, 200))
+rockRect = rock.get_rect()
+rockRect.left = 50
+rockRect.top = 300
+
 paper = mainImage.subsurface((380, 1030, 1420-380, 1959-1030))
 paper = pygame.transform.scale(paper, (300,250))
+paperRect = paper.get_rect()
+paperRect.left = 50
+paperRect.top = 500
+
+computerChoice = random.choice(("paper", "scissors", "rock"))
+#myChoice = input("Please choose your entity between paper, rock and scissors? ")
+#print("Computer choice: ", computerChoice)
+#print("My choice: ", myChoice)
+
+#if computerChoice == myChoice:
+#    print("Tie!")
+#elif computerChoice == "rock" and myChoice == "paper":
+#    print("You won!")
+#elif computerChoice == "paper" and myChoice == "scissors":
+#    print("You won!")
+#elif computerChoice == "scissors" and myChoice == "rock":
+#    print("You won!")
+#else:
+#    print("You lost!")
 
 isRunning = True
 while isRunning:
@@ -39,11 +70,22 @@ while isRunning:
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 isRunning = False
+        if event.type == MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            if scissorsRect.collidepoint(x, y):
+                myChoice = "scissors"
+                isRunning = False
+            elif rockRect.collidepoint(x,y):
+                myChoice = "rock"
+                isRunning = False
+            elif paperRect.collidepoint(x,y):
+                myChoice = "paper"
+                isRunning = False
          
-    SURFACE.blit(text, (50,50))
-    SURFACE.blit(scissors, (50,100))
-    SURFACE.blit(rock, (50,300))
-    SURFACE.blit(paper, (50,500))
+    SURFACE.blit(text, textRect)
+    SURFACE.blit(scissors, scissorsRect)
+    SURFACE.blit(rock, rockRect)
+    SURFACE.blit(paper, paperRect)
     pygame.display.update()
     
 pygame.quit()
